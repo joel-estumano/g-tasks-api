fb4f4869ec89f68068d2d38df655a41dcf10df37
Joel Estumano <joelestumano@gmail.com>
2025-05-29 20:25:18 -0300

    Feat :sparkles: Configuração swagger.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Redirect } from '@nestjs/common';
+// import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    // constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    // @Get()
+    // getHello(): string {
+    //     return this.appService.getHello();
+    // }
+
+    @Get()
+    @Redirect('/swagger', 302)
+    redirectToSwagger() {
+        return;
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { setupSwagger } from './swagger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    const logger = new Logger('main');
+
+    setupSwagger(app);
+
+    await app.listen(process.env.PORT || 3000);
+    logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
